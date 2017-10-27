7a5b58678fdfeda253b44a54fd311124526c7fd3
Torquoal <sevendaysinthesun@gmail.com>
2017-10-27 23:36:28 +0100

    quit button

--- src/gameloop.py
+++ src/gameloop.py
@@ -24,14 +24,22 @@ jeremy_location_y = 100
 
 jeremyImage = pygame.image.load("jeremy.jpg")
 
+image_size_x = 50
+image_size_y = 50
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x: done = True    
+        
+                
 
     screen.fill(WHITE)
 
     screen.blit(jeremyImage, (jeremy_location_x, jeremy_location_y))
+    
 
     messages.displayIntroMessageOne(screen, 250, intro_message_1_location_y)
     messages.displayIntroMessageTwo(screen, 250, intro_message_2_location_y)
@@ -41,12 +49,15 @@ while not done:
     intro_message_1_location_y -= 1
     intro_message_2_location_y -= 1
     intro_message_3_location_y -= 1
+    image_size_x += 1
+    image_size_y += 1
 
     if intro_message_4_location_y > 250:
         intro_message_4_location_y -= 1
 
     jeremy_location_x += 3
     jeremy_location_y = + 3
+        
 
     pygame.display.flip()
     clock.tick(60)
