d325ad46286d54f415a0f3191a252f8228fa0bf5
Stephen Brown <1106679b@student.gla.ac.uk>
2017-10-27 22:33:47 +0100

    More messages in scroll

--- src/gameloop.py
+++ src/gameloop.py
@@ -10,8 +10,14 @@ pygame.display.set_caption("KEEP TALKING AND NOBODY EXPLODES 2")
 done = False
 clock = pygame.time.Clock()
 
-text_location_x = 250
-text_location_y = 250
+text_location_1_x = 250
+text_location_1_y = 250
+
+text_location_2_x = 250
+text_location_2_y = 500
+
+text_location_3_x = 250
+text_location_3_y = 600
 
 while not done:
 	for event in pygame.event.get():
@@ -21,9 +27,18 @@ while not done:
 	screen.fill((255,255,255))
 	
 	font = pygame.font.SysFont('Calibri', 25, True, False)
-	text = font.render("HELLO SHAUNALD. NEVER GIVE UP\n\n\n\n\n\n\n\n\n\n", True, (0,0,0))
-	screen.blit(text, [text_location_x, text_location_y])
-	text_location_y -= 1
+	text1 = font.render("HELLO SHAUNALD. NEVER GIVE UP", True, (0,0,0))
+	screen.blit(text1, [text_location_1_x, text_location_1_y])
+
+	text2 = font.render("JEREMY SINGER WILL GUIDE YOU", True, (0,0,0))
+	screen.blit(text2, [text_location_2_x, text_location_2_y])
+
+	text3 = font.render("FIND YOUR WAY TO THE TRUE PATH", True, (255,0,0))
+	screen.blit(text3, [text_location_3_x, text_location_3_y])
+
+	text_location_1_y -= 1
+	text_location_2_y -= 1
+	text_location_3_y -= 1
 
 	pygame.display.flip()
 	clock.tick(60)
