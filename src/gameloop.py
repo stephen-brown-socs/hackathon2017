fae5f1c06c4aa24bb80092f5ba1d6a8f4b6bc9a0
Stephen Brown <1106679b@student.gla.ac.uk>
2017-10-27 21:38:52 +0100

    Add text

--- src/gameloop.py
+++ src/gameloop.py
@@ -10,12 +10,21 @@ pygame.display.set_caption("KEEP TALKING AND NOBODY EXPLODES 2")
 done = False
 clock = pygame.time.Clock()
 
+text_location_x = 250
+text_location_y = 250
+
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 	
 	screen.fill((255,255,255))
+	
+	font = pygame.font.SysFont('Calibri', 25, True, False)
+	text = font.render("HELLO SHAUNALD. NEVER GIVE UP", True, (0,0,0))
+	screen.blit(text, [text_location_x, text_location_y])
+
 	pygame.display.flip()
 	clock.tick(60)
 	
+pygame.quit()
